5f589aff543d8448069930d72672b352e24cbcd0
wintanagb <wintanaltc@gmail.com>
2020-12-15 17:05:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here!let ul = document.createElement('ul')
+
+main.remove('main')
+
+var newHeader = document.createElement('h1')
+newHeader.id="victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+
